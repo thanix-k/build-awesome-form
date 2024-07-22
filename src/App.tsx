727592d0fd45b26fad5cd5cef251b07d94b4df08
sahilalam0 <sahil.mansoor@anywhere.co>
2024-07-22 20:33:05 +0530

    fix: hovering label triggering input hover

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Radio } from "./components/Radio.tsx";
 function App() {
   return (
     <div className="flex items-center justify-center w-full h-dvh bg-slate-900 flex-col gap-4">
-      <Input label="What is your company called?" placeholder="Organisation name" />
+      <Input id="organisation_name" label="What is your company called?" placeholder="Organisation name" />
       <Radio name="teamType" label="Company" />
       <Button>Create team</Button>
     </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,6 @@
-export default function Input({ label,...props }) {
-    return <label className="flex flex-col">
-        {label && <span className="text-white mb-2">{label}</span>}
-        <input className="p-2 outline-none bg-black/10 rounded-xl outline outline-1 outline-offset-0 outline-white/10 hover:outline-white/20 focus:outline-[#796bfa] text-white" {...props} />
-  </label>;
+export default function Input({id, label,...props }) {
+    return <div className="flex flex-col">
+        {label && <label className="text-white mb-2 pointer-events-none" for={id}>{label}</label>}
+        <input id={id} className="p-2 outline-none bg-black/10 rounded-xl outline outline-1 outline-offset-0 outline-white/10 hover:outline-white/20 focus:outline-[#796bfa] text-white" {...props} />
+  </div>;
 }
