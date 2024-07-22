6ddd7968bba46af2c893a23f16e3b5fdaccfa547
sahilalam0 <sahil.mansoor@anywhere.co>
2024-07-22 20:15:04 +0530

    fix: children props not reauired

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,4 +1,4 @@
-export function Radio({ children,label, ...props }) {
+export function Radio({ label, ...props }) {
     return <label className="flex items-center gap-2 rounded-xl bg-white/10 hover:bg-white/20 py-2 px-3">
         <input type="radio" className="appearance-none outline-none size-5 bg-black/30 rounded-full border border-solid border-[#545251] focus:ring-2 focus:ring-[#796bfa] focus:ring-offset-2 focus:ring-offset-[#545251] checked:border-[#796bfa] checked:border-solid checked:border-[6px] checked:bg-white" {...props} />
         <span className="text-white">{label}</span>
