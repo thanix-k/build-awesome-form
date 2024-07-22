70a1d761b1963a244de7824ccf4adc955bfe4473
sahilalam0 <sahil.mansoor@anywhere.co>
2024-07-22 21:34:55 +0530

    feat: added checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/Button.tsx";
+import { CheckBox } from "./components/CheckBox.tsx";
 import Input from "./components/Input.tsx";
 import { Radio } from "./components/Radio.tsx";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="flex items-center justify-center w-full h-dvh bg-slate-900 flex-col gap-4">
       <Input id="organisation_name" label="What is your company called?" placeholder="Organisation name" />
       <Radio name="teamType" label="Company" />
+      <CheckBox label="I agree to the terms and conditions" />
       <Button>Create team</Button>
     </div>
   );
