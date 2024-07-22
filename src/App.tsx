c1360fdbb317eef666a8ff5bea694fc693902dd8
sahilalam0 <sahil.mansoor@anywhere.co>
2024-07-22 20:13:45 +0530

    feat: added radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Button from "./components/Button.tsx";
 import Input from "./components/Input.tsx";
+import { Radio } from "./components/Radio.tsx";
 
 function App() {
   return (
-    <div className="flex items-center justify-center w-full h-dvh bg-gradient-to-r from-slate-900 to-purple-900 flex-col gap-4">
+    <div className="flex items-center justify-center w-full h-dvh bg-slate-900 flex-col gap-4">
       <Input label="What is your company called?" placeholder="Organisation name" />
+      <Radio name="teamType" label="Company" />
       <Button>Create team</Button>
     </div>
   );
