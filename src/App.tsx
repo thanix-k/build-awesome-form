7e3078517221f8f18a0f4fbcd80abe7145dcdc28
sahilalam0 <sahil.mansoor@anywhere.co>
2024-07-22 23:13:00 +0530

    feat: added required attribute

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
     <div className="flex items-center justify-center w-full h-dvh bg-black flex-col gap-4">
       <form className="w-full max-w-lg bg-gradient-to-r from-slate-900 to-purple-900 p-10 flex flex-col gap-5 rounded-xl">
         <h1 className="text-white text-3xl font-serif font-normal mb-4">Tell us about your team</h1>
-        <Input id="organisation_name" label="What is your company called?" placeholder="Organisation name" />
-        <Input id="website_url" label="What is your website url (optional)?" placeholder="https://" />
+        <Input id="organisation_name" label="What is your company called?" placeholder="Organisation name" required />
+        <Input id="website_url" label="What is your website url (optional)?" placeholder="https://" type="url" required/>
         <FieldSet legend="I’m signing up for Tiptap because">
           {checkBoxes.map((label) => (
             <CheckBox label={label} />
