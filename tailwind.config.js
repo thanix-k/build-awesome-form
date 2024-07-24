3c170825ca2d351c95db48baed3fbf42f27a6bc3
SMITH RAKESH <smith.muni@anywhere.co>
2024-07-24 22:56:20 +0530

    added tickmark svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'tick': "url('./assets/TickMark.svg')",
+      }
+    },
   },
   plugins: [],
 }
